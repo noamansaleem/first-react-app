57a42a209cc89caeb657b3097b42f8188074730e
noamansaleem <noamantech@gmail.com>
2021-01-04 18:05:56 +0500

    first from git desktop

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Noaman Saleem" age={32} /></div>,
   document.getElementById('root')
 );
 
